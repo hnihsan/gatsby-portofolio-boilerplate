b87d528371aefdacdb6047b9c783be572ce515c8
Helmi Nur Ihsan <helmi.n.ihsan@gmail.com>
2024-03-15 02:04:17 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby x Contentful`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "mLZnCDvG2hiGu_-zlbWnubVIz_jaWbFmUZX513mPv8M",
+      "spaceId": "vlq9g90h1sfs"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-google-gtag", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
